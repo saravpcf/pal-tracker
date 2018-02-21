a71159f4811fb699d06c808efffca038a05a9737
Yem Huynh <yem.huynh@gmail.com>
2018-02-21 09:17:37 -0800

    pushed to cf

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -2,6 +2,8 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -9,4 +11,5 @@ public class PalTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
+
 }
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+
+    public String HELLO;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}")String msg ) {
+      this.HELLO = msg;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return HELLO;
     }
 }
